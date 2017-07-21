bf8157afef011982096c7827f2112c1c9270c814
FiorellaQA <fiorella_90_17@hotmail.com>
2017-07-21 15:48:24 -0500

    Añadiendo estilos

--- src/assets/js/components/choiceOption.js
+++ src/assets/js/components/choiceOption.js
@@ -1,26 +1,29 @@
 'use strict';
 
 const ChoiceOption = (update) => {
-  const section = $('<section></section>');
+  const section = $('<section class="choiceOption_bg container-fluid"></section>');
 
-  const divDetails = $('<div></div>');
-  const title = $('<h2>REAL PLAZA</h2>');
-  const subtitle = $('<h2>19 | centros comerciales EN TODO EL PERÚ</h2>');
-  const detailsRP = $('<p>Descubre todo lo que tenemos para ti</p>');
+  const divDetails = $('<div class="row"></div>');
+  const colTexto = $('<div class="col-xs-12"></div>');
+  const title = $('<h2 class="localizacion__title">Encuentra tu centro comercial más cercano</h2>');
+  const subtitle = $('<p class="localizacion__subtitle">Vive la experiencia de comprar usando tu localización</p>');
 
-  const divChoiceOption = $('<div></div>');
-  const btnChoiceRP = $('<button>Ingresa a tu Real Plaza preferido</button>');
-  const btnUseLocation = $('<button>Prefiero usar mi ubicación</button>');
+  const divChoiceOption = $('<div class="row"></div>');
+  const colBtn = $('<div class="col-xs-12 choiceOption_groupBtn"></div>');
+  const btnChoiceRP = $('<button class="btn btn-connect btn-lg uppercase">Ubícame</button>');
+  const btnUseLocation = $('<p class="text">Listado de centros comerciales</p>');
 
-  // section.append(HeaderAll('Elige una opción',1,update));
-  section.append(divDetails);
-  divDetails.append(title);
-  divDetails.append(subtitle);
-  divDetails.append(detailsRP);
+  divChoiceOption.append(colBtn);
+  colBtn.append(btnChoiceRP);
+  colBtn.append(btnUseLocation);
 
-  section.append(divChoiceOption);
-  divChoiceOption.append(btnUseLocation);
-  divChoiceOption.append(btnChoiceRP);
+  divDetails.append(colTexto);
+  colTexto.append(title);
+  colTexto.append(subtitle);
+
+  section
+      .append(divChoiceOption)
+      .append(divDetails);
 
   btnChoiceRP.on('click', (e) => {
     e.preventDefault();
